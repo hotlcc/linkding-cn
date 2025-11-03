4b8f4108e0accd4508a0e23acd2ef8da5370becf
Allen <hotlcc@qq.com>
2025-11-03 17:11:58 +0800

    1.31.1版本添加书签脚本支持自动提取网页标题和描述

--- resources/1.31.1/templates/bookmarks/bookmarklet.js
+++ resources/1.31.1/templates/bookmarks/bookmarklet.js
@@ -1,8 +1,12 @@
 (function () {
   var bookmarkUrl = window.location;
+  var title = document.querySelector('title')?.textContent || document.querySelector(`meta[property='og:title']`)?.getAttribute('content') || '';
+  var description = document.querySelector(`meta[name='description']`)?.getAttribute('content') || document.querySelector(`meta[property='og:description']`)?.getAttribute(`content`) || '';
   var applicationUrl = '{{ application_url }}';
-
+  
   applicationUrl += '?url=' + encodeURIComponent(bookmarkUrl);
+  applicationUrl += '&title=' + encodeURIComponent(title);
+  applicationUrl += '&description=' + encodeURIComponent(description);
   applicationUrl += '&auto_close';
 
   window.open(applicationUrl);
